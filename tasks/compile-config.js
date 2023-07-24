c3b07ffd1639583f22481b164ec8e5aa811a402f
Philipp <philippfromme@outlook.com>
2023-07-24 10:26:17 +0200

    feat: add `compile-config` task to create resolver

---
+++ tasks/compile-config.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const compileConfig = require('bpmnlint/lib/support/compile-config');
+
+const config = {
+  extends: 'plugin:camunda-compat/all'
+};
+
+compileConfig(config).then(code => fs.writeFileSync('lib/compiled-config.js', code));
